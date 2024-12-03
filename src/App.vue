78fb245b1d0084e3423c0bcde55ef30184433807
Garima Pandey <drgp945@gmail.com>
2024-12-03 14:49:11 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <search-input @gifs-fetched="onGifsFetched"/>
+
+  <gif-list :gifs="gifs"/>
+</template>
+
+<script>
+
+import SearchInput from "@/components/SearchInput";
+import GifList from "@/components/GifList";
+
+export default {
+name: 'App',
+components: {
+  GifList,
+  SearchInput
+},
+data() {
+  return {
+    gifs: [],
+  }
+},
+methods: {
+  onGifsFetched(result) {
+    console.log(result);
+    this.gifs = result.data
+  }
+}
+}
+</script>
+
+<style>
+* {
+box-sizing: border-box;
+}
+body {
+padding: 50px
+}
+
+#app {
+width: 800px;
+margin: 0 auto;
+}
+</style>
---
+++ src/components/GifList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="gifList">
+    <gif v-for="gif in gifs" :gif="gif" :key="gif.id"/>
+  </div>
+</template>
+
+<script>
+import Gif from "@/components/Gif";
+export default {
+name: "GifList",
+  components: {Gif},
+  props: ['gifs']
+}
+</script>
+
+<style scoped>
+ 
+#gifList {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 20px;
+}
+
+.gif {
+  width: 240px;
+  height: 240px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  border-radius: 8px;
+}
+</style>
+
